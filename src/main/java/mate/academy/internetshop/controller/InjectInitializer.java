89348446530fa7ece7b991ea3d4d7f756d02c2bf
IvanKrivoruchkoHub <39123857+IvanKrivoruchkoHub@users.noreply.github.com>
2020-01-14 22:35:54 +0200

    Hw17 (#6)
    * hw 17 solution, used logger
    
    * added RuntimeExeption in Injector and InjectInitializer,
    moved log directory
    
    * added loger to injector

--- src/main/java/mate/academy/internetshop/controller/InjectInitializer.java
+++ src/main/java/mate/academy/internetshop/controller/InjectInitializer.java
@@ -3,14 +3,20 @@ package mate.academy.internetshop.controller;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import mate.academy.internetshop.lib.Injector;
+import org.apache.log4j.Logger;
 
 public class InjectInitializer implements ServletContextListener {
+
+    private static final Logger LOGGER = Logger.getLogger(InjectInitializer.class);
+
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         try {
+            LOGGER.info("Start injectionDependency");
             Injector.injectDependency();
         } catch (IllegalAccessException e) {
-            e.printStackTrace();
+            LOGGER.error("Can't initialized all dependencies ", e);
+            throw new RuntimeException(e);
         }
     }
 
