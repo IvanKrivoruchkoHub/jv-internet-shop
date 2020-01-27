d127d0602b95514471eb43cc22f4bd12ec2b9208
ivan <Ivan.krivoruchko.1999@gmail.com>
2020-01-27 17:02:21 +0200

    added DataProcessingExeption

--- src/main/java/mate/academy/internetshop/controller/AddItemController.java
+++ src/main/java/mate/academy/internetshop/controller/AddItemController.java
@@ -5,13 +5,18 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import mate.academy.internetshop.exceptions.DataProcessingExeption;
 import mate.academy.internetshop.lib.anotations.Inject;
 import mate.academy.internetshop.model.Item;
 import mate.academy.internetshop.service.ItemService;
+import org.apache.log4j.Logger;
 
 public class AddItemController extends HttpServlet {
     @Inject
     private static ItemService itemService;
+
+    private static Logger logger = Logger.getLogger(AddItemController.class);
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
@@ -25,7 +30,13 @@ public class AddItemController extends HttpServlet {
         Item item = new Item();
         item.setName(req.getParameter("name"));
         item.setPrice(Double.valueOf(req.getParameter("price")));
-        itemService.create(item);
+        try {
+            itemService.create(item);
+        } catch (DataProcessingExeption dataProcessingExeption) {
+            logger.error(dataProcessingExeption);
+            req.setAttribute("errorMsg", dataProcessingExeption.getMessage());
+            req.getRequestDispatcher("/WEB-INF/views/error.jsp").forward(req, resp);
+        }
         req.setAttribute("item", item);
         req.getRequestDispatcher("/WEB-INF/views/item.jsp").forward(req, resp);
     }
